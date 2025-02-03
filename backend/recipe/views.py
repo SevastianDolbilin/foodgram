2df5954fd7595f30e1a34631a15456b818016e09
Sevastian Dolbilin <basilisk.immortal@gmail.com>
2025-02-04 00:16:46 +0200

    JoJoGold

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -157,5 +157,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         response = HttpResponse(
             "\n".join(shopping_list), content_type="text/plain"
         )
-        response["Content-Disposition"] = 'attachment; filename="shopping_cart.txt"'
+        response[
+            "Content-Disposition"
+        ] = 'attachment; filename="shopping_cart.txt"'
         return response
