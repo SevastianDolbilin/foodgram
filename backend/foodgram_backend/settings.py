88b1db54ec2c425702160ecc30887d632b45405a
Sevastian Dolbilin <basilisk.immortal@gmail.com>
2025-02-06 00:25:35 +0200

    LetsApi

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -111,7 +111,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-    "DEFAULT_PAGINATION_CLASS": "recipe.paginations.CustomPagination",
+    "DEFAULT_PAGINATION_CLASS": "api.paginations.CustomPagination",
     "PAGE_SIZE": 6,
 }
 
