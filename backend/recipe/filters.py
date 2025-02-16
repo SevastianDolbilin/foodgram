93ed63a52b7addc553616fb78f6c1a33e4601ddf
Sevastian Dolbilin <basilisk.immortal@gmail.com>
2025-02-16 04:51:01 +0200

    Fix25

--- backend/recipe/filters.py
+++ backend/recipe/filters.py
@@ -1,4 +1,4 @@
-from django_filters import ModelMultipleChoiceFilter
+from django_filters import MultipleChoiceFilter
 from django_filters import rest_framework as filters
 
 from .models import Recipe, Tag
@@ -9,10 +9,9 @@ class RecipeFilter(filters.FilterSet):
     Фильтр для фильтрации по тегам, авторам, списку покупок и избранному.
     """
 
-    tags = ModelMultipleChoiceFilter(
-        queryset=Tag.objects.all(),
-        field_name="tags",
-        to_field_name="slug",
+    tags = filters.MultipleChoiceFilter(
+        choices=[(tag.slug, tag.name) for tag in Tag.objects.all()],
+        field_name="tags__slug",  # Используем слаги для фильтрации
         distinct=True
     )
 
@@ -24,6 +23,13 @@ class RecipeFilter(filters.FilterSet):
     class Meta:
         model = Recipe
         fields = ["author", "tags", "is_in_shopping_cart", "is_favorited"]
+
+    def filter_by_tags(self, queryset, name, value):
+        """
+        Фильтрует рецепты по тегам.
+        """
+        tags = self.request.query_params.getlist("tags")
+        return queryset.filter(tags__slug__in=tags).distinct()
 
     def filter_in_shopping_cart(self, queryset, name, value):
         """
